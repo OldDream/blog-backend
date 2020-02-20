ca8c554ede19e6342d4b6a121a77d385d5b3e6c7
黄宇宁 <huangyuning@lingkehudong.com>
2020-02-21 01:23:10 +0800

    add：文章列表页get接口

--- app/controller/admin/index.js
+++ app/controller/admin/index.js
@@ -14,12 +14,14 @@ class AdminController extends Controller {
     const sql = `SELECT article.id as id,
       article.title as title,
       article.introduction as introduction,
-      article.created_time as created_time,
+      FROM_UNIXTIME(article.created_time,'%Y-%m-%d %H:%i:%s') as created_time,
       article.view_count as view_count,
       type.typeName as typeName FROM article LEFT JOIN type ON article.type_id = type.id`
     
       const result = await this.app.mysql.query(sql)
-      this.ctx.body = result
+      this.ctx.body = {
+        data: result
+      }
   }
 }
 
