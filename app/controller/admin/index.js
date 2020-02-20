b0ccdaf0ff19744a40e0e6f805a0b14bf3953c60
黄宇宁 <huangyuning@lingkehudong.com>
2020-02-20 17:43:26 +0800

    add: 文章数据库

--- app/controller/admin/index.js
+++ app/controller/admin/index.js
@@ -5,9 +5,21 @@ const Controller = require('egg').Controller;
 class AdminController extends Controller {
   async index() {
     const { ctx } = this;
-    let DBresult = await this.app.mysql.get("blog_content", {})
+    // let DBresult = await this.app.mysql.get("blog_content", {})
     // console.log(DBresult)
-    ctx.body = DBresult
+    ctx.body = 'blog admin api'
+  }
+
+  async getArticleList() {
+    const sql = `SELECT article.id as id,
+      article.title as title,
+      article.introduction as introduction,
+      article.created_time as created_time,
+      article.view_count as view_count,
+      type.typeName as typeName FROM article LEFT JOIN type ON article.type_id = type.id`
+    
+      const result = await this.app.mysql.query(sql)
+      this.ctx.body = result
   }
 }
 
--- app/routers/admin.js
+++ app/routers/admin.js
@@ -1,4 +1,5 @@
 module.exports = app => {
   const {router, controller} = app;
   router.get('/admin/index', controller.admin.index.index)
+  router.get('/admin/getArticleList', controller.admin.index.getArticleList)
 } 
