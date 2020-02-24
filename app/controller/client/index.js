716fb0a8f8f8dda307c4197bbaaa49f73a349141
黄宇宁 <huangyuning@lingkehudong.com>
2020-02-24 18:40:55 +0800

    fix：按分类获取文章列表

--- app/controller/client/index.js
+++ app/controller/client/index.js
@@ -27,7 +27,7 @@ class ClientController extends Controller {
     console.log('typeid:' + typeId)
     const sql = `SELECT article.id as id,
     article.title as title,
-    article.content as content,
+    article.introduction as introduction,
     FROM_UNIXTIME(article.created_time,'%Y-%m-%d %H:%i:%s') as created_time,
     article.view_count as view_count,
     type.typeName as typeName FROM article LEFT JOIN type ON article.type_id = type.id WHERE article.type_id=${typeId}`
